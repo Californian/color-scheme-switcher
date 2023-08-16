3faa051e1cf9aac1f02d5e97453ef15b93d9264a
Californian <1079539+Californian@users.noreply.github.com>
2023-08-16 15:02:18 -0700

    Update example to demonstrate optional provider props.

--- src/examples/ColorSchemeApp/App.tsx
+++ src/examples/ColorSchemeApp/App.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 // If running outside of this repository, replace with the following line.
 // import { ColorSchemeSwitcherProvider } from "color-scheme-switcher";
 import { ColorSchemeSwitcherProvider } from "@/.";
+
 import { ColorSchemeOptionsToggler } from "@/examples/ColorSchemeApp/components";
 
 interface ColorSchemeAppProps {}
 
 const ColorSchemeApp: React.FC<ColorSchemeAppProps> = () => {
+  // Set a default color scheme for the app.
+  const DEFAULT_COLOR_SCHEME = "light";
+
+  // Usually this `_colorScheme` state variable would be used in a component
+  // library's theme provider. If your component library can use the
+  // `colorSchemeIsLight` boolean as-is, wrap the theme provider in a component
+  // that calls the `useColorSchemeSwitcher` hook and passes it to the theme.
+  const [_colorScheme, setColorScheme] = useState<"dark" | "light">(
+    DEFAULT_COLOR_SCHEME
+  );
+
+  // A callback to set the theme provider's color scheme variable when
+  // `color-scheme-switcher`'s color scheme state changes.
+  const setColorSchemeIsLightCallback = (colorSchemeIsLight: boolean) =>
+    setColorScheme(colorSchemeIsLight ? "light" : "dark");
+
   return (
-    <ColorSchemeSwitcherProvider>
+    <ColorSchemeSwitcherProvider
+      defaultColorSchemeIsLight={DEFAULT_COLOR_SCHEME === "light"}
+      setColorSchemeIsLightCallback={setColorSchemeIsLightCallback}
+    >
       <ColorSchemeOptionsToggler />
     </ColorSchemeSwitcherProvider>
   );
